4b5fb8fe793e14b727a417620c2f0cbbdcbed12e
Prabhash Rai <prabhash.rai@elucidata.io>
2022-08-27 11:07:24 +0530

    created navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,36 +1,86 @@
-import React from "react";
+import './Navbar.css';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { AiTwotoneHome, AiFillContacts } from 'react-icons/ai';
+import { RiProjectorFill } from 'react-icons/ri';
+import { SiRiotgames } from 'react-icons/si';
+import { IoBookSharp } from 'react-icons/io5';
+import { MdWork } from 'react-icons/md';
+import { GiSkills } from 'react-icons/gi';
 
 function Navbar() {
-    return (
-      <div>
-        <ul>
+  let prevId = '/home';
+  
+  useEffect(() => {
+    activeNav();
+  });
+
+  function activeNav () {
+    const page = (window.location.href);
+    const currentPage = (window.location.origin);
+    const activeId = findCurrentPage(currentPage, page);
+
+    document.getElementById(prevId).classList.remove('active');
+    document.getElementById(activeId).classList.add('active');
+    prevId = activeId;
+  }
+
+  function findCurrentPage(currentURL, baseURL){ 
+    let currentPage= "";
+    baseURL.split('').forEach(function(letter, pos){
+      if (letter !== currentURL.charAt(pos))
+      currentPage += letter;         
+    });
+    return currentPage;
+  }
+
+  // const [active, setActive] = useState('home');
+  // setActive = () => {
+
+  // }
+  return (
+    <>
+      <div className='nav'>
+        <ul className='nav-list' onClick={activeNav}>
           <li>
-            <Link to="/home">Home</Link>
+            <Link to="/home">
+                <AiTwotoneHome id='/home' className="icon white-background"/>
+            </Link>
           </li>
           <li>
-            <Link to="/challenge">Play with Me</Link>
+            <Link to="/challenge">
+                <SiRiotgames id='/challenge' className='icon white-background'/>
+            </Link>
           </li>
           <li>
-            <Link to="/skills">Skills</Link>
+            <Link to="/skills">
+                <GiSkills id='/skills' className='icon white-background'/>
+            </Link>
           </li>
           <li>
-            <Link to="/education">Education</Link>
+            <Link to="/education">
+                <IoBookSharp id='/education' className='icon white-background'/>
+            </Link>
           </li>
           <li>
-            <Link to="/projects">Projects</Link>
+            <Link to="/projects" >
+                <RiProjectorFill id='/projects' className='icon white-background'/>
+            </Link>
           </li>
           <li>
-            <Link to="/experience">Experience</Link>
+            <Link to="/experience">
+                <MdWork id='/experience' className='icon white-background'/>
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact">
+                <AiFillContacts id='/contact' className='icon white-background'/>
+            </Link>
           </li>
         </ul>
-          Navbar
       </div>
-    );
-  }
-  
-  export default Navbar;
-  
+    </>
+  );
+}
+
+export default Navbar;
