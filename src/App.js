db6e6a8bb68c025d668efe190d147ffff77808c2
Prabhash Rai <prabhash.rai@elucidata.io>
2022-09-02 10:42:20 +0530

    fixed routing

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import Skills from './Component/Skills/Skills';
 import Contact from './Component/Contact/Contact';
 import Projects from './Component/Projects/Projects';
 import Education from './Component/Education/Education';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, Navigate, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/home' element={<Home />} />
-          <Route exact path='/challenge' element={<PlaywithMe />} />
-          <Route exact path='/skills' element={<Skills />} />
-          <Route exact path='/experience' element={<Experience />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/projects' element={<Projects />} />
-          <Route exact path='/education' element={<Education />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='/challenge' element={<PlaywithMe />} />
+        <Route exact path='/skills' element={<Skills />} />
+        <Route exact path='/experience' element={<Experience />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/projects' element={<Projects />} />
+        <Route exact path='/education' element={<Education />} />
+        <Route path="*" element={<Navigate replace to="/home" />} />
+      </Routes>
     </Router>
     
   );
