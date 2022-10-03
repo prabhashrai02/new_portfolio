a04e72ec992563969cf3a265320caed4daaca25e
Prabhash Rai <prabhash.rai@elucidata.io>
2022-10-03 19:57:51 +0530

    used elastic carousel

--- src/Component/Projects/Projects.js
+++ src/Component/Projects/Projects.js
@@ -1,86 +1,35 @@
-import { useState, useEffect } from "react";
 import Card from './Project-card';
 import { ProjectList } from "./Project-details";
-import { AiFillCaretLeft, AiFillCaretRight, AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
 import "./Project.css";
+import Carousel from 'react-elastic-carousel';
 
 let list = ProjectList.map((project, i) => <Card data={project} key={i}/>);
 
 function Projects() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const carouselInfiniteScroll = () => {
-
-    const last = list.pop();
-    list.unshift(last);
-
-    if (currentIndex === list.length - 1) {
-      return setCurrentIndex(0);
-    }
-    
-    return setCurrentIndex(currentIndex+1);
-  }
-
-  const right = () => {
-
-    const last = list.shift();
-    list.push(last);
-
-    setCurrentIndex(currentIndex + 1);
-
-    if (currentIndex === list.length - 1) setCurrentIndex(0);
-
-  }
-
-  const left = () => {
-
-    const last = list.pop();
-    list.unshift(last);
-
-    setCurrentIndex(currentIndex - 1);
-
-    if (currentIndex === -1) setCurrentIndex(ProjectList.length - 1);
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => carouselInfiniteScroll(), 3000);
-
-    return () => clearInterval(interval);
-  })
-
+  const breakPoints = [
+    {width: 1, itemsToShow: 1},
+    {width: 300, itemsToShow: 2},
+    {width: 768, itemsToShow: 3},
+  ];
   return (
     <>
        <div className="project_section">
-        <div className="project_mobile_button_block" onClick={left}>
-         <AiFillCaretUp className="project_mobile_button"/>
-        </div>
-        <div className="project_button_block" onClick={left}>
-         <AiFillCaretLeft className="project_button"/>
-        </div>
-         <div className='project_list'>
-           {
+        <Carousel breakPoints={breakPoints}>
+          {
             list.map(
               (project, index) => {
                 return (
-                  <div className={index === 1 ? 'active' : 'not-active'} key={index}>
+                  <div key={index}>
                     {project}
                   </div>
                 )
               }
             )
           }
-        </div>
-        <div className="project_button_block" onClick={right}>
-          <AiFillCaretRight className="project_button"/>
-        </div>
-        
-        <div className="project_mobile_button_block" onClick={right}>
-          <AiFillCaretDown className="project_mobile_button"/>
-        </div>
+        </Carousel>
       </div>
     </>
   );
 }
   
 export default Projects;
-  
