a012b3d26b8d6aaae933c7f0b5df68937da5e182
Prabhash Rai <prabhash.rai@elucidata.io>
2022-10-04 23:04:43 +0530

    fixed className error

--- src/Component/Skills/Skills.js
+++ src/Component/Skills/Skills.js
@@ -10,85 +10,85 @@ function Skills() {
             <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
               <img src="https://angular.io/assets/images/logos/angular/angular.svg" alt="angular" className="skill_icon" />
             </a>
-            <span class="tooltiptext">Angular</span>
+            <span className="tooltiptext">Angular</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">Bootstarp</span>
+            <span className="tooltiptext">Bootstarp</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">C</span>
+            <span className="tooltiptext">C</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer" >
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" className="skill_icon" />
             </a>
-            <span class="tooltiptext">C++</span>
+            <span className="tooltiptext">C++</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">CSS</span>
+            <span className="tooltiptext">CSS</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://dart.dev" target="_blank" rel="noreferrer">
               <img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" alt="dart" className="skill_icon" />
             </a>
-            <span class="tooltiptext">Dart</span>
+            <span className="tooltiptext">Dart</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
               <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase"/>
             </a>
-            <span class="tooltiptext">Firebase</span>
+            <span className="tooltiptext">Firebase</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://flutter.dev" target="_blank" rel="noreferrer">
               <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">Flutter</span>
+            <span className="tooltiptext">Flutter</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
               <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">Git</span>
+            <span className="tooltiptext">Git</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">HTML5</span>
+            <span className="tooltiptext">HTML5</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" className="skill_icon" />
             </a>
-            <span class="tooltiptext">Javascript</span>
+            <span className="tooltiptext">Javascript</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" className="skill_icon" />
             </a>
-            <span class="tooltiptext">MySQL</span>
+            <span className="tooltiptext">MySQL</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" className="skill_icon"/>
             </a>
-            <span class="tooltiptext">ReactJS</span>
+            <span className="tooltiptext">ReactJS</span>
           </div>
           <div className="tooltip skill_box">
             <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
               <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" className="skill_icon" />
             </a>
-            <span class="tooltiptext">TyeScript</span>
+            <span className="tooltiptext">TyeScript</span>
           </div>
         </div>
       </div>
