628075e41ec6e24af976d8e1488c3f1c2e444854
Prabhash Rai <prabhash.rai@elucidata.io>
2022-10-05 12:03:34 +0530

    changed theme

--- src/App.js
+++ src/App.js
@@ -7,10 +7,24 @@ import Skills from './Component/Skills/Skills';
 import Contact from './Component/Contact/Contact';
 import Projects from './Component/Projects/Projects';
 import Education from './Component/Education/Education';
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
 import { Routes, Route, Navigate, HashRouter as Router } from 'react-router-dom';
 
 function App() {
+  const particlesInit = useCallback(async (engine) => {
+
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await container;
+  }, []);
+
   return (
+    <>
+
     <Router>
       <div className="brand">"See the good"</div>
       <Navbar />
@@ -26,6 +40,77 @@ function App() {
       </Routes>
     </Router>
     
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          // color: {
+          //   value: "#0d47a1",
+          // },
+        },
+        fpsLimit: 50,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 3,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: ["triangle", "circle"],
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+    </>
   );
 }
 
