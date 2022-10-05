8432ca1459503e3a4feca7e356907d482aa2df33
Prabhash Rai <prabhash.rai@elucidata.io>
2022-10-05 16:36:43 +0530

    fix experience card design

--- src/Component/Experience/Experience-card.js
+++ src/Component/Experience/Experience-card.js
@@ -7,9 +7,11 @@ function ExperienceCard (props) {
             <a href={data.website} target="_blank" rel="noreferrer">
                 <img src={data.logo} alt="company" className="experience_logo"/>
             </a>
-            <h2>{data.designation}</h2>
-            <h4>{data.duration}</h4>
-            <h5>{data.tech_stack}</h5>
+            <div className="head">
+                <h2>{data.designation}</h2>
+                <h4>{data.duration}</h4>
+                <h5>{data.tech_stack}</h5>
+            </div>
             <br />
             <li>{data.description_l1}</li>
             <li>{data.description_l2}</li>
