27176e0ee39b232a38b8b709860643630871157b
Prabhash Rai <prabhashsahaj@gmail.com>
2022-12-08 20:19:03 +0530

    added more projects

--- src/Component/Projects/Project-details.js
+++ src/Component/Projects/Project-details.js
@@ -1,17 +1,24 @@
 export const ProjectList = [
     {
+        name: `Groww Feed App`,
+        image: `https://user-images.githubusercontent.com/73634195/199815021-b4d0fef6-9b79-484f-adde-b9d5b3c32939.png`,
+        tech_stack: `ReactJs, HTML, CSS, JS`,
+        github_link: `https://github.com/prabhashrai02/groww-feed-app`,
+        live_link: `https://prabhashrai02.github.io/groww-feed-app/`,
+    },
+    {
         name: `Search Image`,
         image: `https://user-images.githubusercontent.com/73634195/148273357-5d605c96-94bb-42c5-9637-95b48435b731.png`,
         tech_stack: `ReactJs, HTML, CSS, JS`,
         github_link: `https://github.com/prabhashrai02/image_search`,
-        live_link: `https://searchpictures.herokuapp.com/`,
+        live_link: `https://prabhashrai02.github.io/image_search/`,
     },
     {
         name: `Dukaan Slogan Generator`,
         image: `https://user-images.githubusercontent.com/73634195/161261058-66fc2215-0410-4194-a913-451a7a6ca827.png`,
         tech_stack: `ReactJs, HTML, CSS, JS`,
         github_link: `https://github.com/prabhashrai02/dukaan`,
-        live_link: `https://dukaan-for-pc.herokuapp.com/`,
+        live_link: `https://prabhashrai02.github.io/dukaan/`,
     },
     {
         name: `Notes App`,
@@ -32,6 +39,13 @@ export const ProjectList = [
         image: `https://user-images.githubusercontent.com/73634195/154430424-54290b35-787f-4cce-9033-b20426705748.png`,
         tech_stack: `ReactJs, HTML, CSS, JS`,
         github_link: `https://github.com/prabhashrai02/banking_management_-manager-view-`,
-        live_link: `https://indian-bank-manager.herokuapp.com/`,
+        live_link: `https://prabhashrai02.github.io/banking_management_-manager-view-/`,
+    },
+    {
+        name: `Water Tank Visualizer`,
+        image: `https://user-images.githubusercontent.com/73634195/197586973-0872e496-becb-4209-8078-e15b330153a5.png`,
+        tech_stack: `HTML, CSS, JS`,
+        github_link: `https://github.com/prabhashrai02/water_tank_visualizer`,
+        live_link: `https://prabhashrai02.github.io/water_tank_visualizer/`,
     },
 ]
